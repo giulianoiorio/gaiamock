a8e6871d7069ca88cd480dd64496b416599a5b57
giulianoiorio <giuliano.iorio89@gmail.com>
2025-12-01 19:13:50 -0800

    added the class LCtemplate in the variability_tool module

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class CustomBuild(build_py):
 # Setup script
 setup(
     name="gaiamock",
-    version="1.2.1",
+    version="1.2.2",
     description="A package for simulatiing Gaia astrometry at epoch level",
     author="Kareem El-Badry",
     author_email="",
