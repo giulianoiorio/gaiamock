4d702e4cdcc1e39fa856091a3809ccd4a9ecded8
giulianoiorio <giuliano.iorio89@gmail.com>
2025-12-02 11:59:51 -0800

    changed setup versione

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class CustomBuild(build_py):
 # Setup script
 setup(
     name="gaiamock",
-    version="1.2.3",
+    version="1.2.4",
     description="A package for simulatiing Gaia astrometry at epoch level",
     author="Kareem El-Badry",
     author_email="",
